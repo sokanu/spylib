35dc25083a62f9871c52da4cc677c0f4b6225ef9
Alek Hrycaiko <alek@sokanu.com>
2019-06-14 10:54:42 -0700

    more tests

--- src/tests/test_request.py
+++ src/tests/test_request.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 from ..request import Request
-from ..exceptions import RefreshException
+from ..exceptions import RefreshException, LoginException
 import datetime
+import json
 import jwt
 import unittest
+import responses
 
 
 class TestRequest(unittest.TestCase):
@@ -62,7 +64,7 @@ class TestRequest(unittest.TestCase):
         assert req.base_url
         assert req.refresh_token == "billy"
 
-    def test_init_with_bad_access_no_refresh_token_throws_exception(self):
+    def test_bad_access_token_no_refresh_token_throws_exception(self):
         """
         Given:
             - a bad access token
@@ -90,7 +92,8 @@ class TestRequest(unittest.TestCase):
                 secret=secret,
             )
 
-    def test_init_with_good_access_no_refresh_token_throws_exception(self):
+    @responses.activate
+    def test_bad_access_no_refresh_token_server_throws_exception(self):
         """
         Given:
             - a bad access token
@@ -98,9 +101,71 @@ class TestRequest(unittest.TestCase):
             - a secret
             - an algorithm
             - no refresh token.
+            - a mocked refresh endpoint that fails.
         When:
-            - a request object is built.
+            - a request obj built and server fails with a non 200 status code.
         Outcome:
             - throws a RefreshException
         """
-        pass
+        secret = "1234"
+        algorithm = "HS256"
+        access_token = jwt.encode(
+            {"exp": datetime.datetime.now() + datetime.timedelta(-30)},
+            secret,
+            algorithm=algorithm,
+        ).decode("utf-8")
+        responses.add(
+            responses.POST, "https://localhost:8000/api/v1/tokens", status=500
+        )
+        with self.assertRaises(RefreshException):
+            Request(
+                "https://localhost:8000",
+                access_token=access_token,
+                algorithm=algorithm,
+                secret=secret,
+            )
+
+    @responses.activate
+    def test_login_raises_login_exception_on_failure(self):
+        """
+        Given:
+            - no tokens
+            - a bad response.
+        When:
+            - login called
+        Outcome:
+            - LoginException raised.
+
+        """
+        responses.add(responses.POST, "https://localhost:8000/api/v1/login", status=500)
+        with self.assertRaises(LoginException):
+            Request("https://localhost:8000").login("fake", "fake")
+
+    @responses.activate
+    def test_login_succeeds_when_server_201(self):
+        """
+        Given:
+            - no tokens.
+            - a good response.
+        When:
+            - login called
+        Outcome:
+            - LoginException raised.
+
+        """
+
+        def request_callback(request):
+            resp_body = {"access_token": "5678"}
+            headers = {"set-cookie": "refresh_token=1234;"}
+            return (200, headers, json.dumps(resp_body))
+
+        responses.add_callback(
+            responses.POST,
+            "https://localhost:8000/api/v1/login",
+            callback=request_callback,
+            content_type="application/json",
+        )
+        resp = Request("https://localhost:8000").login("fake", "fake")
+
+        assert resp.get("access_token") == "5678"
+        assert resp.get("refresh_token") == "1234"
