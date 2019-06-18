823e33bb8f3b9f773663346cdc49147406a60d39
Alek Hrycaiko <alek@sokanu.com>
2019-06-18 15:22:04 -0700

    use setup with proper version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="Spylib",
-    version="0.0dev",
+    version="v0.0.1",
     packages=["src"],
     long_description=open("README.md").read(),
     install_requires=["requests>=2.22.0", "PyJWT>=1.7.1"],
