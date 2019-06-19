4f2725f8afb379116f7e969ddc1ab437262ac540
Alek Hrycaiko <alek@sokanu.com>
2019-06-19 11:10:54 -0700

    up to 4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="Spylib",
-    version="v0.0.2",
+    version="v0.0.4",
     packages=["src"],
     long_description=open("README.md").read(),
     install_requires=["requests>=2.21.0", "PyJWT>=1.7.1"],
