ef0a4b666fae501de8a6f46012f7d59d276f236b
Alek Hrycaiko <alek@sokanu.com>
2019-06-19 14:23:58 -0700

    use setup tools

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 from distutils.core import setup
+import setuptools
+
 
 setup(
     name="Spylib",
     version="v0.0.4",
-    packages=["src"],
+    packages=setuptools.find_packages(),
     long_description=open("README.md").read(),
     install_requires=["requests>=2.0.0", "PyJWT>=1.7.1"],
 )
