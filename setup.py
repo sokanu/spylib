46ac972b037c006b327370213c23ea219758c49d
Alek Hrycaiko <alek@sokanu.com>
2019-07-05 14:58:21 -0700

    upgrdt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setup(
     name="Spylib",
-    version="v0.0.14",
+    version="v0.0.15",
     packages=setuptools.find_packages(),
     long_description=open("README.md").read(),
     install_requires=["requests>=2.0.0", "PyJWT>=1.7.1", "future>=0.17.0"],
