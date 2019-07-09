1d158f5919a4affdabe3a4559c333596e85c79e1
ryanisnan <ryanisnan@gmail.com>
2019-07-09 15:44:16 -0700

    update exceptions

--- spylib/exceptions.py
+++ spylib/exceptions.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import logging
 
 
 class AuthCredentialException(Exception):
@@ -7,4 +6,4 @@ class AuthCredentialException(Exception):
 
 
 class MethodException(Exception):
-    logging.debug("An invalid method was passed to the requests library.")
+    pass
