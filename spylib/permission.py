740c77845e9b0215fad18d31f821d3ca9d7a7e2f
Alek Hrycaiko <alek@sokanu.com>
2019-12-09 13:48:16 -0800

    add doc string to has_permission

--- spylib/permission.py
+++ spylib/permission.py
@@ -1,3 +1,4 @@
+"""Permissions file for Spylib."""
 from __future__ import absolute_import
 from jwt import decode
 from jwt import DecodeError
@@ -5,6 +6,16 @@ from jwt import ExpiredSignatureError
 
 
 def has_permission(jwt, uuid, permission, JWT_ALGORITHM, JWT_SECRET):
+    """
+    Permission check on JWT for a given uuid.
+
+    Args:
+        jwt(str): A JSON Web Token that is deciphered and checked.
+        uuid(str): The UUID that is checked for a given permission.
+        permission(str): The permission that is checked to exist on the given uuid.
+        JWT_ALGORITHM(str): The algorithm used for encoding and decoding the jwt.
+        JWT_SECRET(str): The secret used for encoding and decoding the jwt.
+    """
     try:
         decoded = decode(jwt, JWT_SECRET, algorithms=[JWT_ALGORITHM])
         services = decoded.get("services", None)
