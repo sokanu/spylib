787737a36f5e1d36766fee8ff6a78577e1c49c62
Alek Hrycaiko <alekhrycaiko@gmail.com>
2020-07-20 11:42:24 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+"""Setup package for Spylib."""
 from __future__ import absolute_import
 from distutils.core import setup
 import setuptools
@@ -5,7 +6,7 @@ import setuptools
 
 setup(
     name="Spylib",
-    version="v0.0.19",
+    version="v0.1.5",
     packages=setuptools.find_packages(),
     long_description=open("README.md").read(),
     install_requires=["requests>=2.0.0", "PyJWT>=1.7.1", "future>=0.17.0"],
