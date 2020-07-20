50ff39eb6c2f33f62066b5de01f3fa86089cc64c
Alek Hrycaiko <alekhrycaiko@gmail.com>
2020-07-20 11:49:28 -0700

    bump patch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setup(
     name="Spylib",
-    version="v0.1.5",
+    version="v0.1.6",
     packages=setuptools.find_packages(),
     long_description=open("README.md").read(),
     install_requires=["requests>=2.0.0", "PyJWT>=1.7.1", "future>=0.17.0"],
